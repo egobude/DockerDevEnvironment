284b8d37bb4c33fb362ed2ca03fe4aa361c2f9e1
Benedikt Schmitz <benedikt.schmitz@avency.de>
2016-11-08 13:43:06 +0100

    [TASK] Pass container name
    You are now able to starting a bash in any running
    container by passing the container name.

--- bin/plugins/bash.sh
+++ bin/plugins/bash.sh
@@ -4,6 +4,6 @@ set -u
 set -e
 
 bash() {
-    log "Starting bash in php container"
-    docker-compose exec --user=1000:33 php /bin/bash
+    log "Starting a bash in the given container"
+    docker-compose exec --user=1000:33 $@ /bin/bash
 }
